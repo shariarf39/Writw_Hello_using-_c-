ca3f6f90b6bd1dcbed2eb1a5baec4a445deb1630
shariarf39 <110589927+shariarf39@users.noreply.github.com>
2023-02-14 21:45:24 +0600

    Writw_Hello_using _c++
    Writw_Hello_using _c++

---
+++ pattren1.cpp
@@ -0,0 +1,245 @@
+
+//Create By Fahim Shariar
+//con (+8801892382840 (Whatsapp))
+
+#include<bits/stdc++.h>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+
+   system("color 03");
+
+int n = 15;
+int raw;
+cout<<endl;
+cout<<endl;
+cout<<endl;
+cout<<endl;
+cout<<endl;
+cout<<endl;
+
+for( raw = 1; raw<=n; raw++){
+
+      for(int col = 1; col<=n; col++){
+
+        if( col==1){
+            cout<< "                  ";
+        }else cout<< "";
+      }
+        //H
+    for(int col = 1; col<=n; col++){
+
+
+        if( raw == 8|| col==1 || col==n){
+            cout<< "C";
+
+        }else {cout<< " ";}
+
+
+
+
+
+    }
+
+//// Space
+      for(int col = 1; col<=n; col++){
+        if( col==1){
+            cout<< "   ";
+        }else cout<< "";
+
+
+
+
+    }
+
+      //EEEEEE
+      for(int col = 1; col<=n; col++){
+        if( raw == 8|| col==1 || raw==n|| raw==1){
+            cout<< "C";
+
+        }else {cout<< " ";}
+
+
+
+
+
+    }
+
+
+      //LLLLLLLL
+
+
+      for(int col = 1; col<=n; col++){
+        if( col==1){
+            cout<< "  ";
+        }else cout<< "";
+
+
+
+
+    }
+
+      //EEEEEE
+      for(int col = 1; col<=n; col++){
+        if(col==1 || raw==n){
+            cout<< "C";
+
+        }else {cout<< " ";}
+
+
+
+
+
+    }
+
+//LLLLLLLL
+
+      for(int col = 1; col<=n; col++){
+        if( col==1){
+            cout<< "   ";
+        }else cout<< "";
+
+
+
+
+    }
+
+      //EEEEEE
+      for(int col = 1; col<=n; col++){
+        if(col==1 || raw==n){
+            cout<< "C";
+
+        }else {cout<< " ";}
+
+
+
+
+
+    }
+
+
+      for(int col = 1; col<=n; col++){
+        if( col==1){
+            cout<< "  ";
+        }else cout<< "";
+
+
+
+
+    }
+
+      //EEEEEE
+      for(int col = 1; col<=n; col++){
+        if(col==1 || raw==n || col == n || raw== 1){
+            cout<< "C";
+
+        }else {cout<< " ";}
+
+
+
+
+
+    }
+
+
+    // WORLD
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////
+     cout <<endl;
+
+
+     //LLLLLLLLLLL
+
+}
+
+
+///
+
+cout<<endl;
+cout << endl;
+/*
+
+int a =7;
+int i ,j, row=10;
+row++;
+int k, count=7;
+
+for (int i = 1; i <=row;i++){
+        for(int j = 1 ; j <=row-i; j++){
+
+
+            if(j==row-i|| j ==1 ){
+                cout << "C";
+            }else {cout<< " ";}
+
+
+        }
+        for(j=3;j<=i*2;j++){
+            cout << " ";
+        }
+        for(j=1; j <=row-i;j++){
+            if(j==1||j==row-i){
+                cout << "C";
+            }else{
+            cout <<" ";}
+        }
+
+        //////////
+
+          for(int col = 1; col<=n; col++){
+        if( col==1){
+            cout<< " ";
+        }else cout<< "";
+
+
+
+
+    }
+
+
+
+    /////////
+
+//////////////////////////
+        cout << endl;
+
+}
+
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+getch();
+
+
+}
